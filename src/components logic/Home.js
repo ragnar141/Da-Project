70bfc3e019ce854c97c0f7022fd73d509b0f2d96
Amir Yakhin <amir.yakhin94@google.com>
2024-09-09 12:49:16 -0500

    updated home component

--- src/components logic/Home.js
+++ src/components logic/Home.js
@@ -2,7 +2,21 @@ import React from 'react';
 import '../components css/Home.css';
 
 const Home = () => {
-  return <div className="section">Home</div>;
+  return (
+    <div className="home-container">
+      <div className="title-section">
+        <div className="section welcome-section">
+          Welcome to
+        </div>
+        <div className="section uni-section">
+          UNI
+        </div>
+      </div>
+      <div className="section description-section">
+        Uni is an online platform dedicated to providing free self-education resources and affordable courses. Our interdisciplinary approach fosters a holistic understanding of the world, helping learners connect ideas across fields.
+      </div>
+    </div>
+  );
 };
 
 export default Home;
